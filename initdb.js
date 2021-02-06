1bdc3403601b0d1142c93b857e4906a778ad9ac4
Georgina Torralba <georgina.torralba@gmail.com>
2021-02-06 23:05:25 +0100

    Initialitation th DataBase with dataTest with async await done,

--- initdb.js
+++ initdb.js
@@ -7,51 +7,26 @@ const testAdvertisements = require('./advertisements.json');
 //load model
 const Advertisement = require('./model/Advertisement')
 
-function getAdvertisements(){
-    
-    testAdvertisements.advertisements.forEach(testAdvertisement =>{
-        //console.log(`este es el anuncio ${testAdvertisement}`)
-        let advertisementToAdd = new Advertisement(testAdvertisement);
-        try{
-            advertisementToAdd.save(function(err, advertisementCreated){
-                if(err) {
-                    return console.log(err);
-                };
-                console.log(`Advertisement ${advertisementCreated} created`);
-            })
-        }catch(err){
-            console.log(err)
-        }
-    });
+seedDB().catch(err => console.log(err));
 
+async function getAdvertisements(){
+    let totalCreated = 0;
+    for (let advertisement of testAdvertisements.advertisements){
+        let advertisementToAdd = new Advertisement(advertisement);
+        await advertisementToAdd.save();
+        totalCreated ++
+    }    
+    return(totalCreated)
 }
-function disconnectDB(){
-    
-    try{
-        mongoose.connection.close()
-    }catch(err){
-        console.log('err');
-    }
-}
+
+
 async function seedDB(){
-    
-    try{
-        //connect to db
-        await mongoose.connection.dropCollection('advertisements', function(){
-            console.log('dropCollection success');
-        });
-
-        await getAdvertisements()
-        await disconnectDB();
-    }catch(err){
-        console.log(`An error success ${err}`);
-        return err;
-    }
-    
+    //connect to db
+    await mongoose.connection.dropCollection('advertisements', function(){
+        console.log('dropCollection success');
+    });
+    const adverstisementCreated = await getAdvertisements()
+    console.log(`Has been created ${adverstisementCreated} advertisements successful`);
+    mongoose.connection.close()
+    console.log('Disconnet to db')
 }
-
-seedDB()
-//disconnectDB()
-
-
-
