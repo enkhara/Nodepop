9947ff368a09be74bbf3ae3e6495e71ba5c8dded
Georgina Torralba <georgina.torralba@gmail.com>
2021-02-07 13:37:17 +0100

    Model finish with statics

--- model/Advertisement.js
+++ model/Advertisement.js
@@ -1,16 +1,40 @@
 'use strict';
 
+const { query } = require('express');
 const mongoose = require('mongoose');
 
 //Schema
 
 const advertisementSchema = mongoose.Schema({
-    name: String,
-    sale: Boolean,
+    name: { type: String, index: true },
+    sale: { type: Boolean, index:true},
     price: Number,
     image: String,
     tags: [String]
 });
 
+//methods
+
+//metohod lista
+advertisementSchema.statics.lista = function(filter, limit, skip, fields, sort){
+    const query = Advertisement.find(filter);
+    query.limit(limit);
+    query.skip(skip);
+    query.select(fields);
+    query.sort(sort);
+    return query.exec();
+}
+
+advertisementSchema.statics.tagsList = function(){
+    const queryTags = Advertisement.find();
+    queryTags.distinct('tags');
+    return queryTags.exec()
+}
+/*
+advertisementSchema.methods.sold = function() {
+    this.sale = true;
+    return this.save();
+}
+*/
 const Advertisement = mongoose.model('Advertisement', advertisementSchema);
 module.exports = Advertisement;
